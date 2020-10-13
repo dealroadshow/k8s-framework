da9d444d2913114aad95047091d759809a5287b1
Pasechnik Bogdan <bogdan.pasichnyk@finsight.com>
2020-10-13 14:17:40 +0300

    job maker

---
+++ src/Core/Job/JobInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dealroadshow\K8S\Framework\Core\Job;
+
+use Dealroadshow\K8S\API\Batch\Job;
+use Dealroadshow\K8S\Framework\Core\ManifestInterface;
+use Dealroadshow\K8S\Framework\Core\Pod\PodTemplateSpecInterface;
+
+interface JobInterface extends PodTemplateSpecInterface, ManifestInterface
+{
+    public function backoffLimit(): ?int;
+    public function activeDeadlineSeconds(): ?int;
+    public function ttlSecondsAfterFinished(): ?int;
+    public function completions(): ?int;
+    public function manualSelector(): ?bool;
+    public function parallelism(): ?int;
+    public function configureJob(Job  $job): void;
+}
