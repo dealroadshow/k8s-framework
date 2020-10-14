c9ca7f8803de96b3879b5e69e2c11bfe8126c16b
Pasechnik Bogdan <bogdan.pasichnyk@finsight.com>
2020-10-14 17:05:38 +0300

    job maker

--- src/Renderer/YamlRenderer.php
+++ src/Renderer/YamlRenderer.php
@@ -9,7 +9,29 @@ class YamlRenderer extends AbstractRenderer
     public function render(\JsonSerializable $object): string
     {
         $data = $this->withoutNullValues($object);
+        $inline = $this->calcDumpInline($data);
 
-        return Yaml::dump($data, 7, 2);
+        return Yaml::dump($data, $inline, 2);
+    }
+
+    private function calcDumpInline($array): int
+    {
+        $depth = $this->calcDepth($array);
+
+        return max(7, $depth - 1);
+    }
+
+    private function calcDepth(array $array): int {
+        $maxDepth = 1;
+        foreach ($array as $value) {
+            if (is_array($value)) {
+                $depth = $this->calcDepth($value) + 1;
+                if ($depth > $maxDepth) {
+                    $maxDepth = $depth;
+                }
+            }
+        }
+
+        return $maxDepth;
     }
 }
