e76c7a52e9f4674f0bd30499681833452550a653
Pasechnik Bogdan <bogdan.pasichnyk@finsight.com>
2020-10-23 10:58:26 +0300

    tags

--- src/App/AppProcessor.php
+++ src/App/AppProcessor.php
@@ -4,23 +4,35 @@
 namespace Dealroadshow\K8S\Framework\App;
 
 use Dealroadshow\K8S\Framework\Core\ManifestProcessor;
+use Dealroadshow\K8S\Framework\Dumper\Context\ContextInterface;
 use Dealroadshow\K8S\Framework\Registry\ManifestRegistry;
 
 class AppProcessor
 {
     private ManifestRegistry $manifestRegistry;
     private ManifestProcessor $manifestProcessor;
+    /**
+     * @var ContextInterface
+     */
+    private ContextInterface $context;
 
-    public function __construct(ManifestRegistry $manifestRegistry, ManifestProcessor $manifestProcessor)
+    public function __construct(ManifestRegistry $manifestRegistry, ManifestProcessor $manifestProcessor, ContextInterface $context)
     {
         $this->manifestRegistry = $manifestRegistry;
         $this->manifestProcessor = $manifestProcessor;
+        $this->context = $context;
     }
 
     public function process(AppInterface $app): void
     {
         $query = $this->manifestRegistry->query();
         $query->app($app);
+        if($tags = $this->context->includeTags()) {
+            $query->includeTags($tags);
+        }
+        if($tags = $this->context->excludeTags()) {
+            $query->excludeTags($tags);
+        }
         foreach ($query->execute() as $manifest) {
             $this->manifestProcessor->process($manifest, $app);
         }
