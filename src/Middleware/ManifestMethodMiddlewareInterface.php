cb8f4410d06df5630c4e864d24660b5266905e71
petr-buchyn <petr.buchyn@finsight.com>
2020-12-09 20:00:12 +0200

    Make ManifestMethodMiddlewareInterface::priority() method static

--- src/Middleware/ManifestMethodMiddlewareInterface.php
+++ src/Middleware/ManifestMethodMiddlewareInterface.php
@@ -7,6 +7,6 @@ use Dealroadshow\K8S\Framework\Core\ManifestInterface;
 interface ManifestMethodMiddlewareInterface
 {
     public function beforeMethodCall(ManifestInterface $manifest, string $methodName, array $params);
-    public function priority(): int;
     public function supports(ManifestInterface $manifest, string $methodName, array $params): bool;
+    public static function priority(): int;
 }
