793bf82dc24324735461c60f5cfd9e872c7a73bc
petr-buchyn <petr.buchyn@finsight.com>
2021-04-04 00:14:04 +0300

    Fix in ReflectionUtil

--- src/Util/ReflectionUtil.php
+++ src/Util/ReflectionUtil.php
@@ -4,9 +4,18 @@ namespace Dealroadshow\K8S\Framework\Util;
 
 class ReflectionUtil
 {
-    public static function sameSignature(\ReflectionFunctionAbstract $methodA, \ReflectionFunctionAbstract $methodB): bool
+    public static function sameSignature(\ReflectionFunctionAbstract $methodA, \ReflectionFunctionAbstract $methodB, bool $ignoreNullsInReturnTypes = true): bool
     {
-        if (strval($methodA->getReturnType()) !== strval($methodB->getReturnType())) {
+        $returnTypeA = (string)$methodA->getReturnType();
+        $returnTypeB = (string)$methodB->getReturnType();
+        if ($ignoreNullsInReturnTypes) {
+            $returnTypeA = ltrim($returnTypeA, '?');
+            $returnTypeB = ltrim($returnTypeA, '?');
+            $returnTypeA = str_replace('|null', '', $returnTypeA);
+            $returnTypeB = str_replace('|null', '', $returnTypeB);
+        }
+
+        if ($returnTypeA !== $returnTypeB) {
             return false;
         }
 
