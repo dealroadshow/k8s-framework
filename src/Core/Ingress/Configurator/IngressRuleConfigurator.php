185de91025b95549731fc1fddcdec12f4bd15586
petr-buchyn <petr.buchyn@finsight.com>
2021-09-08 00:53:32 +0300

    Defferent ways to create HTTPIngressPath depending on K8S API version

--- src/Core/Ingress/Configurator/IngressRuleConfigurator.php
+++ src/Core/Ingress/Configurator/IngressRuleConfigurator.php
@@ -2,11 +2,13 @@
 
 namespace Dealroadshow\K8S\Framework\Core\Ingress\Configurator;
 
+use Composer\Semver\Comparator;
 use Dealroadshow\K8S\Data\HTTPIngressPath;
 use Dealroadshow\K8S\Data\IngressRule;
 use Dealroadshow\K8S\Framework\App\AppInterface;
 use Dealroadshow\K8S\Framework\Core\Ingress\Http\PathType;
 use Dealroadshow\K8S\Framework\Util\ManifestReferenceUtil;
+use Dealroadshow\K8S\K8SApi;
 
 class IngressRuleConfigurator
 {
@@ -34,7 +36,7 @@ class IngressRuleConfigurator
             );
         }
 
-        $ingressPath = new HTTPIngressPath($pathType->toString());
+        $ingressPath = $this->createPath($pathType);
         $this->rule->http()->paths()->add($ingressPath);
 
         if ($path) {
@@ -46,5 +48,17 @@ class IngressRuleConfigurator
             app: $this->app,
             manifestReferenceUtil: $this->manifestReferenceUtil
         );
+    }
+
+    private function createPath(PathType $pathType): HTTPIngressPath
+    {
+        if (Comparator::greaterThanOrEqualTo(K8SApi::VERSION, 'v1.22.0')) {
+            return new HTTPIngressPath($pathType->toString());
+        }
+
+        $path = new HTTPIngressPath();
+        $path->setPathType($pathType->toString());
+
+        return $path;
     }
 }
