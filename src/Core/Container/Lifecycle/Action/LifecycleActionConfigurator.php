17e95ffbab9ae8e480863a67da9fb7588b113b92
yafanasiev <yevhenii.afanasiev@finsight.com>
2022-09-22 16:58:56 +0300

    fix: k8s 1.23 LifecycleHandler

--- src/Core/Container/Lifecycle/Action/LifecycleActionConfigurator.php
+++ src/Core/Container/Lifecycle/Action/LifecycleActionConfigurator.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Dealroadshow\K8S\Framework\Core\Container\Lifecycle\Action;
 
-use Dealroadshow\K8S\Data\Handler;
+use Dealroadshow\K8S\Data\LifecycleHandler;
 
 class LifecycleActionConfigurator
 {
     use ActionConfiguratorTrait;
 
-    private Handler $handler;
+    private LifecycleHandler $handler;
 
-    public function __construct(Handler $handler)
+    public function __construct(LifecycleHandler $handler)
     {
         $this->handler = $handler;
     }
