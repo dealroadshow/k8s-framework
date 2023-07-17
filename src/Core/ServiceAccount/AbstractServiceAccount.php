24bd22a242b60ddf042b86a8955c11a1f0a13d67
petr-buchyn <petr.buchyn@finsight.com>
2023-07-17 15:00:26 +0300

    Added methods to AbstractServiceAccount

--- src/Core/ServiceAccount/AbstractServiceAccount.php
+++ src/Core/ServiceAccount/AbstractServiceAccount.php
@@ -6,12 +6,22 @@ namespace Dealroadshow\K8S\Framework\Core\ServiceAccount;
 
 use Dealroadshow\K8S\API\ServiceAccount;
 use Dealroadshow\K8S\Framework\Core\AbstractManifest;
+use Dealroadshow\K8S\Framework\Core\ServiceAccount\Configurator\ImagePullSecretsConfigurator;
+use Dealroadshow\K8S\Framework\Core\ServiceAccount\Configurator\SecretsConfigurator;
 
 abstract class AbstractServiceAccount extends AbstractManifest implements ServiceAccountInterface
 {
     public function automountServiceAccountToken(): bool
     {
         return false;
+    }
+
+    public function imagePullSecrets(ImagePullSecretsConfigurator $imagePullSecrets): void
+    {
+    }
+
+    public function secrets(SecretsConfigurator $secrets): void
+    {
     }
 
     public function configureServiceAccount(ServiceAccount $serviceAccount): void
