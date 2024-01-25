3f362092eca7356216ff3f579a8dde5e394af5bb
petr-buchyn <petr.buchyn@finsight.com>
2024-01-25 17:27:38 +0200

    Fixed event name

--- src/Event/ManifestsRenderedEvent.php
+++ src/Event/ManifestsRenderedEvent.php
@@ -6,7 +6,7 @@ namespace Dealroadshow\K8S\Framework\Event;
 
 class ManifestsRenderedEvent
 {
-    public const NAME = 'dealroadshow_k8s.manifests.processed';
+    public const NAME = 'dealroadshow_k8s.manifests.rendered';
 
     public function __construct(public readonly array $renderedManifests)
     {
